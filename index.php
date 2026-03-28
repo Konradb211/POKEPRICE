98805f4718933150e0212a86e66968bd5c54dc6a
Konradb211 <konradbas1234@gmail.com>
2026-03-28 23:53:03 +0100

    Testing pokemontcg api

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
 
-use Dotenv\Dotenv;
+require __DIR__ . "/vendor/autoload.php";
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
+use GuzzleHttp\Client;
+use function GuzzleHttp\json_decode;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$client = new Client([
+    "base_uri" => "https://api.pokemontcg.io/v2/",
+]);
+
+$response = $client->get("cards?q=name:ditto&pageSize=10");
+
+$data = json_decode($response->getBody(), true);
+
+$firstPokemon = $data["data"];
 ?>
 
 <!DOCTYPE html>
@@ -21,8 +31,24 @@ $dotenv->load();
 </head>
 
 <body>
-    <?php
-    ?>
+    <div class="wrapper">
+        <?php foreach ($firstPokemon as $pokemons) {
+            $name = $pokemons["name"];
+            $smallImg = $pokemons["images"]["small"];
+            $largeImage = $pokemons["images"]["large"];
+            $prices = $pokemons["tcgplayer"]["prices"] ?? [];
+            echo "<div class='item'>";
+            echo "<img src='$smallImg' alt='$name' />";
+            foreach ($prices as $price) {
+                echo "<p class='price'>" .
+                    "cena: " .
+                    $price["market"] .
+                    "$" .
+                    "</p>";
+            }
+            echo "</div>";
+        } ?>
+    </div>
 </body>
 
 </html>

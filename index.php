cacb4aed24155c2b03880f374d5e566e4de5250c
Konradb211 <konradbas1234@gmail.com>
2026-03-28 17:24:42 +0100

    Add .env libary to project

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+
+use Dotenv\Dotenv;
+
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 ?>
 
 <!DOCTYPE html>

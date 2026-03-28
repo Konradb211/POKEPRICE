5e2702b9fec6a9729c16be910e284bcc069e9eb9
Konradb211 <konradbas1234@gmail.com>
2026-03-28 17:19:04 +0100

    First pokeprice commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/styles.css">
+    <title>POKEPRICE</title>
+</head>
+
+<body>
+    <?php
+    ?>
+</body>
+
+</html>
